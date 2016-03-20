591df871eb322cdcd8e563341c4c6409eb96a84b
TheDragonsShadow <carl.razmus@outlook.com>
2016-03-20 12:39:44 +0100

    * Updated layout in PlayersActivity   - Removed "Back" button   - Added ListView that displays player names

--- app/src/main/java/com/fraz/dartlog/PlayersActivity.java
+++ app/src/main/java/com/fraz/dartlog/PlayersActivity.java
@@ -3,37 +3,60 @@ package com.fraz.dartlog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PlayersActivity extends ActionBarActivity implements View.OnClickListener {
+    private ArrayList<String> playersNamesStringArray;
+    private ArrayAdapter<String> arrayStringAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_players);
 
-        Button backButton = (Button) findViewById(R.id.back_button);
-        backButton.setOnClickListener(this);
         Button readyButton = (Button) findViewById(R.id.ready_button);
         readyButton.setOnClickListener(this);
+
+        Button addPlayerButton = (Button) findViewById(R.id.add_player_button);
+        addPlayerButton.setOnClickListener(this);
+
+        /* create a listView that contains all players who will join the game */
+        ListView myListView = (ListView) findViewById(R.id.players_listView);
+        playersNamesStringArray = new ArrayList<>();
+        arrayStringAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, playersNamesStringArray);
+        myListView.setAdapter(arrayStringAdapter);
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
-            case R.id.back_button:
-                startMainActivity();
-                break;
             case R.id.ready_button:
                 startPlayActivity();
+                break;
+            case R.id.add_player_button:
+                addPlayerNameToPlayerListView();
                 break;
         }
     }
 
-    private void startMainActivity() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+    private void addPlayerNameToPlayerListView()
+    {
+        EditText editText = (EditText) findViewById(R.id.player_input_textfield);
+        String playerName = editText.getText().toString();
+        Log.d("PlayerName", playerName);
+
+        arrayStringAdapter.add(playerName);
+
+        //playersNamesStringArray.add(playerName);
+        //arrayStringAdapter.notifyDataSetChanged();
+
     }
 
     private void startPlayActivity() {
