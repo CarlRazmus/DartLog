3452906ecb12646679418e94ee5f7e1ce49539b2
Filip Källström <filip.kallstrom@gmail.com>
2016-03-26 22:53:36 +0100

    Implemented checkout hints in X01.

--- app/src/main/java/com/fraz/dartlog/X01.java
+++ app/src/main/java/com/fraz/dartlog/X01.java
@@ -1,26 +1,56 @@
 package com.fraz.dartlog;
 
-import android.content.Context;
+import android.app.Activity;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class X01 {
 
-    private final Context context;
+    private final Activity context;
     private final ArrayList<PlayerData> players;
+    private final String checkoutUnavailableText = "Checkout Unavailable";
     private Integer currentPlayerIdx;
     private Integer startingScore;
     private PlayerData winner;
+    private TextView checkoutView;
+    private Map<Integer, String> checkouts = new HashMap<>();
 
-    public X01(Context context, ArrayList<PlayerData> players, Integer x) {
+    public X01(Activity context, ArrayList<PlayerData> players, Integer x) {
         this.context = context;
         this.players = players;
 
         currentPlayerIdx = 0;
         startingScore = x*100 + 1;
         resetScores();
+
+        initCheckoutView();
+        initCheckoutMap();
+    }
+
+    private void initCheckoutMap() {
+        InputStream inputStream = context.getResources().openRawResource(R.raw.checkout_chart);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        try {
+            String line = reader.readLine();
+            while (line != null) {
+                String[] checkout = line.split(",");
+                checkouts.put(Integer.parseInt(checkout[0]), checkout[1]);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void enterScore(Integer score) {
@@ -32,18 +62,48 @@ public class X01 {
             } else {
                 currentPlayer.setScore(newScore);
             }
-
-            if (newScore == 0) {
-                setWinner(currentPlayer);
-                showWinnerToast();
-            }
-            else
-                nextPlayer();
+            updateGameState();
         }
     }
 
     public Boolean isDone() {
         return winner != null;
+    }
+
+    public void newLeg() {
+        setActivePlayer(0);
+        winner = null;
+        resetScores();
+        updateCheckoutHint();
+    }
+
+    private void updateGameState() {
+        PlayerData currentPlayer = players.get(currentPlayerIdx);
+        Integer currentScore = currentPlayer.getScore();
+        if (currentScore == 0) {
+            setWinner(currentPlayer);
+            showWinnerToast();
+        }
+        else {
+            nextPlayer();
+            updateCheckoutHint();
+        }
+    }
+
+    private void updateCheckoutHint() {
+        Integer currentScore = players.get(currentPlayerIdx).getScore();
+        String checkoutHint = checkouts.get(currentScore);
+        if (checkoutHint == null) {
+            checkoutHint = checkoutUnavailableText;
+        }
+
+        checkoutView.setText(checkoutHint);
+    }
+
+    private void initCheckoutView() {
+        checkoutView = (TextView) context.findViewById(R.id.game_hint);
+        checkoutView.setText(checkoutUnavailableText);
+        checkoutView.setVisibility(View.VISIBLE);
     }
 
     private void setWinner(PlayerData currentPlayer) {
@@ -79,12 +139,6 @@ public class X01 {
 
     public int getCurrentPlayer(){
         return currentPlayerIdx;
-    }
-
-    public void newLeg() {
-        setActivePlayer(0);
-        winner = null;
-        resetScores();
     }
 
     private void resetScores() {
