d0d71ab7cec1b2c2a5bb9d8aa7887eac31fd43f6
TheDragonsShadow <carl.razmus@outlook.com>
2016-03-31 21:54:04 +0200

    * Updated error handling and UI graphics in PlayersActivity

--- app/src/main/java/com/fraz/dartlog/PlayersActivity.java
+++ app/src/main/java/com/fraz/dartlog/PlayersActivity.java
@@ -1,20 +1,22 @@
 package com.fraz.dartlog;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class PlayersActivity extends ActionBarActivity implements View.OnClickListener {
     private ArrayList<String> playersNames;
-    private ArrayAdapter<String> arrayStringAdapter;
+    private SelectedPlayersListAdapter arrayStringAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +31,12 @@ public class PlayersActivity extends ActionBarActivity implements View.OnClickLi
 
         /* create a listView that contains all players who will join the game */
         ListView myListView = (ListView) findViewById(R.id.players_listView);
+
         playersNames = new ArrayList<>();
         /* add names for debug purposes, remove when app is finished */
         playersNames.add("Razmus Lindgren");
         playersNames.add("Filip Källström");
-        arrayStringAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, playersNames);
+        arrayStringAdapter = new SelectedPlayersListAdapter(this, playersNames);
         myListView.setAdapter(arrayStringAdapter);
     }
 
@@ -45,23 +48,54 @@ public class PlayersActivity extends ActionBarActivity implements View.OnClickLi
                 break;
             case R.id.add_player_button:
                 addPlayerNameToPlayerListView();
+                clearInputTextField();
                 break;
         }
+    }
+
+    private void clearInputTextField() {
+        EditText editText = (EditText) findViewById(R.id.player_input_textfield);
+        editText.setText("");
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
     }
 
     private void addPlayerNameToPlayerListView()
     {
         EditText editText = (EditText) findViewById(R.id.player_input_textfield);
         String playerName = editText.getText().toString();
-        Log.d("PlayerName", playerName);
 
-        arrayStringAdapter.add(playerName);
+        if(!playerName.equals("")) {
+            arrayStringAdapter.add(playerName);
+        }
+        else{
+            showEmptyTextErrorToast();
+        }
+    }
+
+    private void showEmptyTextErrorToast() {
+        showToast("must write something!");
+    }
+
+    private void showMustAddPlayersErrorToast() {
+        showToast("Its kinda hard to play dart without any players!");
+    }
+
+    private void showToast(CharSequence text) {
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
 
     private void startPlayActivity() {
+        if(playersNames.size() == 0){
+            showMustAddPlayersErrorToast();
+        }
+
         Intent intent = new Intent(this, PlayActivity.class);
 
         intent.putStringArrayListExtra("playerNames", playersNames);
         startActivity(intent);
     }
+
 }
---
+++ app/src/main/java/com/fraz/dartlog/SelectedPlayersListAdapter.java
@@ -0,0 +1,51 @@
+package com.fraz.dartlog;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class SelectedPlayersListAdapter extends ArrayAdapter<String> {
+
+    private List<String> playerNames;
+    private Activity context;
+
+    public SelectedPlayersListAdapter(Activity context, List<String> playerNames) {
+        super(context, R.layout.game_player_list_item, playerNames);
+
+        this.context = context;
+        this.playerNames = playerNames;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listItem = convertView;
+
+        if (convertView == null) {
+            listItem = inflater.inflate(R.layout.select_players_list_item, parent, false);
+        }
+
+        TextView playerNameView = (TextView) listItem.findViewById(R.id.select_players_list_item_name);
+        playerNameView.setText(playerNames.get(position));
+
+        Button removeButton = (Button) listItem.findViewById(R.id.remove_player_button);
+        removeButton.setText("Remove");
+
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                remove(getItem(position));
+                notifyDataSetChanged();
+            }
+        });
+
+        return listItem;
+    }
+}
