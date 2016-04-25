6e7376f1ff3f8e2732912cab418a2ad5dab4dbbd
Filip Källström <filip.kalls@gmail.com>
2016-04-25 21:45:24 +0200

    Added hamcrest and mockito as dependencies to tests. Added test file for the CheckoutChart class

---
+++ app/src/test/java/com/fraz/dartlog/CheckoutChartTest.java
@@ -0,0 +1,45 @@
+package com.fraz.dartlog;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CheckoutChartTest {
+
+    @Mock
+    private Context mockContext;
+    @Mock
+    private Resources mockResource;
+
+    private static final String TEST_DATA = "120,T20 20 D20\n90,T18 D18";
+    private CheckoutChart checkoutChart;
+
+    @Before
+    public void setUp() throws Exception {
+        InputStream testInput = new ByteArrayInputStream(TEST_DATA.getBytes());
+        when(mockContext.getResources()).thenReturn(mockResource);
+        when(mockResource.openRawResource(R.raw.checkout_chart)).thenReturn(testInput);
+    }
+
+    @Test
+    public void testGetCheckoutText() throws Exception {
+        checkoutChart = new CheckoutChart(mockContext, R.raw.checkout_chart);
+        assertThat(checkoutChart.getCheckoutText(90), equalTo("T18 D18"));
+        assertThat(checkoutChart.getCheckoutText(120), equalTo("T20 20 D20"));
+        assertThat(checkoutChart.getCheckoutText(121), equalTo("N/A"));
+        assertThat(checkoutChart.getCheckoutText(161), equalTo("N/A"));
+    }
+}
