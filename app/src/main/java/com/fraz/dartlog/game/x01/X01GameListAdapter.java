b77da6c8970868e0806316acae1add3d64a44858
CarlRazmus <carl.razmus@outlook.com>
2016-10-28 20:33:29 +0200

    * created base GameListAdapter class, this separated X01 and Random game list adapters into two separate classes.

---
+++ app/src/main/java/com/fraz/dartlog/game/x01/X01GameListAdapter.java
@@ -0,0 +1,45 @@
+package com.fraz.dartlog.game.x01;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.fraz.dartlog.R;
+import com.fraz.dartlog.game.GameListAdapter;
+
+import java.util.LinkedList;
+
+public class X01GameListAdapter extends GameListAdapter {
+
+    private X01 game;
+
+    public X01GameListAdapter(X01 game) {
+        super(game);
+    }
+
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        super.onBindViewHolder(holder, position);
+        final X01PlayerData player = (X01PlayerData) this.game.getPlayer(position);
+        updateCheckoutView(player, holder);
+    }
+
+    private void updateCheckoutView(X01PlayerData player, ViewHolder holder) {
+        if (player.getScore() == 0) {
+            holder.checkout_view.setVisibility(View.VISIBLE);
+            holder.checkout.setText("");
+            holder.checkoutLabel.setText(R.string.result_win);
+        }
+        else if (holder.getAdapterPosition() == game.getCurrentPlayerIdx()) {
+            holder.checkout.setText(player.getCheckoutText());
+            if (player.mustDoubleOut()) {
+                holder.checkoutLabel.setText(R.string.double_out);
+            } else {
+                holder.checkoutLabel.setText(R.string.single_out);
+            }
+            holder.checkout_view.setVisibility(View.VISIBLE);
+        } else {
+            holder.checkout_view.setVisibility(View.GONE);
+        }
+    }
+}
