9174e58de38ad1a3dc3155cf38aba815b50b5995
CarlRazmus <carl.razmus@outlook.com>
2017-01-04 18:20:34 +0100

    * Initial commit of adding Google Sign In. * Added sign in button with login functionality

--- app/src/main/java/com/fraz/dartlog/MainActivity.java
+++ app/src/main/java/com/fraz/dartlog/MainActivity.java
@@ -3,16 +3,32 @@ package com.fraz.dartlog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.fraz.dartlog.game.setup.SetupActivity;
 import com.fraz.dartlog.statistics.ProfileListActivity;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity
+        implements  GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 1001;
+    private static final String TAG = "SignInActivity";
+    private TextView loginStatusTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +47,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         PreferenceManager.setDefaultValues(this, R.xml.x01_preferences, false);
         randomButton.setOnClickListener(this);
+
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        // Set the dimensions of the sign-in button.
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(this);
+
+        loginStatusTextView = (TextView) findViewById(R.id.loginStatusTextView);
     }
 
     @Override
@@ -55,6 +92,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "SIGN IN RESULT: " + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            loginStatusTextView.setText("Logged in as \"" + acct.getDisplayName() + "\"");
+
+        } else {
+            // Signed in unsuccessfully, show unauthenticated UI.
+            loginStatusTextView.setText("Failed to log in \n " +
+                    "status: " + result.getStatus() + "\n");
+        }
+        loginStatusTextView.setVisibility(View.VISIBLE);
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("CONNECTION STATUS:", "CONNECTION FAILED");
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
@@ -67,7 +143,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.play_random_button:
                 startPlayersActivity("random");
                 break;
-
+            case R.id.sign_in_button:
+                signIn();
+                break;
         }
     }
 
