34d9d90b1850a0f409e024cacad27169a53d9aeb
Filip Källström <filip.kallstrom@gmail.com>
2017-07-04 21:54:25 +0200

    Created a custom view of the items in the match history list

---
+++ app/src/main/java/com/fraz/dartlog/statistics/MatchItemView.java
@@ -0,0 +1,94 @@
+package com.fraz.dartlog.statistics;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.text.format.DateUtils;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import com.fraz.dartlog.R;
+import com.fraz.dartlog.game.GameData;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class MatchItemView extends CardView {
+
+    private TextView dateView;
+    private TextView gameType;
+    private TextView result;
+    private TextView players;
+    private GameData game;
+    private String playerName;
+    private Context context;
+
+    public MatchItemView(Context context) {
+        super(context);
+        initView(context);
+    }
+
+    public MatchItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context);
+    }
+
+    private void initView(Context context) {
+        this.context = context;
+        LayoutInflater.from(context).inflate(R.layout.match_history_item, this);
+        gameType = (TextView) findViewById(R.id.match_history_game_type);
+        players = (TextView) findViewById(R.id.match_history_players);
+        result = (TextView) findViewById(R.id.match_history_result);
+        dateView = (TextView) findViewById(R.id.match_history_date);
+    }
+
+    public void setGame(GameData game, String playerName) {
+        this.game = game;
+        this.playerName = playerName;
+        initGame();
+    }
+
+    private void initGame() {
+        gameType.setText(game.getGameType().toUpperCase());
+        initPlayers();
+        initResult();
+        initDate();
+        invalidate();
+    }
+
+
+    private void initPlayers() {
+        int numberOfPlayers = game.getNumberOfPlayers();
+        String text;
+        if (numberOfPlayers == 1)
+            text = String.format(Locale.getDefault(), "%d playerName", numberOfPlayers);
+        else
+            text = String.format(Locale.getDefault(), "%d players", numberOfPlayers);
+        players.setText(text);
+    }
+
+    private void initResult() {
+        if (game.getPlayer(playerName).equals(game.getWinner())) {
+            result.setText(R.string.win);
+            result.setTextColor(context.getResources().getColor(R.color.green_win));
+        }
+        else {
+            result.setText(R.string.loss);
+            result.setTextColor(context.getResources().getColor(R.color.red_loss));
+        }
+    }
+
+    private void initDate() {
+        Date date = game.getDate().getTime();
+        if (DateUtils.isToday(date.getTime()))
+            dateView.setText(DateUtils.getRelativeTimeSpanString(date.getTime(),
+                    Calendar.getInstance().getTimeInMillis(),
+                    DateUtils.SECOND_IN_MILLIS,
+                    DateUtils.FORMAT_ABBREV_ALL));
+        else
+            dateView.setText(new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).
+                    format(date));
+    }
+}
--- app/src/main/java/com/fraz/dartlog/statistics/MatchRecyclerViewAdapter.java
+++ app/src/main/java/com/fraz/dartlog/statistics/MatchRecyclerViewAdapter.java
@@ -3,21 +3,13 @@ package com.fraz.dartlog.statistics;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
-import android.text.format.DateUtils;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.fraz.dartlog.R;
 import com.fraz.dartlog.game.GameData;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
-import java.util.Date;
-import java.util.Locale;
 
 public class MatchRecyclerViewAdapter extends RecyclerView.Adapter<MatchRecyclerViewAdapter.ViewHolder> {
 
@@ -34,8 +26,7 @@ public class MatchRecyclerViewAdapter extends RecyclerView.Adapter<MatchRecycler
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.match_history_item, parent, false);
+        MatchItemView view = new MatchItemView(context);
         return new ViewHolder(view);
     }
 
@@ -43,10 +34,7 @@ public class MatchRecyclerViewAdapter extends RecyclerView.Adapter<MatchRecycler
     public void onBindViewHolder(final ViewHolder holder, final int position) {
         GameData game = gameData.get(position);
 
-        holder.gameType.setText(game.getGameType().toUpperCase());
-        bindPlayers(holder, game);
-        bindResult(holder, game);
-        bindDate(holder, game);
+        ((MatchItemView) holder.itemView).setGame(game, playerName);
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,57 +46,15 @@ public class MatchRecyclerViewAdapter extends RecyclerView.Adapter<MatchRecycler
             }
         });
     }
-
-    private void bindPlayers(ViewHolder holder, GameData game) {
-        int numberOfPlayers = game.getNumberOfPlayers();
-        String text;
-        if (numberOfPlayers == 1)
-            text = String.format(Locale.getDefault(), "%d player", numberOfPlayers);
-        else
-            text = String.format(Locale.getDefault(), "%d players", numberOfPlayers);
-        holder.players.setText(text);
-    }
-
-    private void bindResult(ViewHolder holder, GameData game) {
-        if (game.getPlayer(playerName).equals(game.getWinner())) {
-            holder.result.setText(R.string.win);
-            holder.result.setTextColor(context.getResources().getColor(R.color.green_win));
-        }
-        else {
-            holder.result.setText(R.string.loss);
-            holder.result.setTextColor(context.getResources().getColor(R.color.red_loss));
-        }
-    }
-
-    private void bindDate(ViewHolder holder, GameData game) {
-        Date date = game.getDate().getTime();
-        if (DateUtils.isToday(date.getTime()))
-            holder.date.setText(DateUtils.getRelativeTimeSpanString(date.getTime(),
-                    Calendar.getInstance().getTimeInMillis(),
-                    DateUtils.SECOND_IN_MILLIS,
-                    DateUtils.FORMAT_ABBREV_ALL));
-        else
-            holder.date.setText(new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).
-                    format(date));
-    }
-
     @Override
     public int getItemCount() {
         return gameData.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public final TextView gameType;
-        public final TextView players;
-        public final TextView result;
-        public final TextView date;
 
         public ViewHolder(View view) {
             super(view);
-            gameType = (TextView) view.findViewById(R.id.match_history_game_type);
-            players = (TextView) view.findViewById(R.id.match_history_players);
-            result = (TextView) view.findViewById(R.id.match_history_result);
-            date = (TextView) view.findViewById(R.id.match_history_date);
         }
     }
 }
