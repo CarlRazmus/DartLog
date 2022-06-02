f7ddc253f217105d90866bb6122c151338468092
Razmus Lindgren <carl.razmus@outlook.com>
2022-06-02 16:22:11 +0200

    Push match data to server

--- app/src/main/java/com/fraz/dartlog/game/x01/X01GameActivity.java
+++ app/src/main/java/com/fraz/dartlog/game/x01/X01GameActivity.java
@@ -14,6 +14,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.widget.Toolbar;
+
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -25,18 +27,38 @@ import com.fraz.dartlog.R;
 import com.fraz.dartlog.databinding.ActivityX01GameBinding;
 import com.fraz.dartlog.game.InputEventListener;
 import com.fraz.dartlog.game.NumPadHandler;
+import com.fraz.dartlog.game.PlayerData;
 import com.fraz.dartlog.util.EventObserver;
 import com.fraz.dartlog.viewmodel.X01GameViewModel;
+
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
 
 public class X01GameActivity extends AppCompatActivity implements
         OnBackPressedDialogFragment.OnBackPressedDialogListener {
 
     private X01GameListAdapter gameListAdapter;
     private X01GameViewModel viewModel;
+    private Socket socket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try {
+            socket = IO.socket("http://192.168.28.157:5000");
+            socket.connect();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
 
         viewModel = ViewModelProviders.of(this).get(X01GameViewModel.class);
         ActivityX01GameBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_x01_game);
@@ -78,6 +100,7 @@ public class X01GameActivity extends AppCompatActivity implements
         viewModel.getGameObservable().observe(this, new Observer<X01>() {
             @Override
             public void onChanged(@Nullable X01 x01) {
+                sendGameDataToServer();
                 gameListAdapter.notifyDataSetChanged();
                 scrollToPlayerInList();
             }
@@ -106,6 +129,46 @@ public class X01GameActivity extends AppCompatActivity implements
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.game_menu, menu);
         return true;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        socket.disconnect();
+    }
+
+    private void sendGameDataToServer()
+    {
+        final String android_id = Settings.Secure.getString(getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+        String winner = "None";
+        if (viewModel.getGame().isGameOver())
+            winner = viewModel.getGame().getWinner().getPlayerName();
+
+        PlayerData player1 = viewModel.getGame().getPlayer(0);
+        PlayerData player2 = viewModel.getGame().getPlayer(1);
+        Map player1data = new HashMap();
+        Map player2data = new HashMap();
+        player1data.put("name", player1.getPlayerName());
+        player2data.put("name", player2.getPlayerName());
+        LinkedList player1list = (LinkedList)player1.getTotalScoreHistory().clone();
+        LinkedList player2list = (LinkedList)player2.getTotalScoreHistory().clone();
+
+        player1list.add(player1.getScore());
+        player2list.add(player2.getScore());
+        player1data.put("matchdata", player1list);
+        player2data.put("matchdata", player2list);
+
+        Map match = new HashMap();
+        match.put("winner", winner);
+        match.put("player1", player1data);
+        match.put("player2", player2data);
+
+        Map data = new HashMap();
+        data.put("device_id", android_id);
+        data.put("match", match);
+
+        socket.emit("matchdata-update-from-android", new JSONObject(data));
     }
 
     private void initListView() {
